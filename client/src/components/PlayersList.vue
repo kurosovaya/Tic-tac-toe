9bcfc1b5d84d081e7930521a7598883191e130d4
Kurosovaya <nonickk9@gmail.com>
2023-10-23 03:06:09 +0300

    fix

---
+++ client/src/components/PlayersList.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Players list
+    </div>
+</template>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -8,15 +8,17 @@ import router from "@/router";
 const login = ref("")
 
 function loginUser() {
-    let body = { "login": login.value }
-    axios.post("/login", body)
-        .then(response => (response.data)).then(
-            data => {
-                store.playerId = data.player_id
-                $cookies.set("playerId", data.player_id)
-                router.push({ name: "Menu" })
-            }
-        )
+    if (login.value) {
+        let body = { "login": login.value }
+        axios.post("/login", body)
+            .then(response => (response.data)).then(
+                data => {
+                    store.playerId = data.player_id
+                    $cookies.set("playerId", data.player_id)
+                    router.push({ name: "Menu" })
+                }
+            )
+    }
 }
 </script>
 
